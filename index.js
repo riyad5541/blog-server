a495152f52d9f0287942cd5d06a799792da3e2d5
riyad5541 <abdullahalriyad5541@gmail.com>
2024-06-05 13:31:21 +0600

    hash password completed

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 // import mongoose from 'mongoose';
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
+import express from 'express';
+// const cors = require('cors')
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('mongodb is connected')
 })
+.catch((err) => {
+  console.log(err);
+});
 
 const app = express()
 const port = 3000
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+app.use('/api/user', userRoutes )
+app.use('/api/auth', authRoutes);
